7dee432cbfeeeced858a9119a2ea856d656de48d
PhoenixWmj <2567343137@qq.com>
2021-08-05 11:15:38 +0800

    在"main"上提交

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <!-- 1.1使用A模块的state数据 -->
+    <p>{{ $store.state.moduleA.username }}</p>
+    <!-- 1.2使用A模块的getters数据 -->
+    <p>{{ $store.getters.newName }}</p>
+    <!-- 2.1使用B模块的state数据 -->
+    <p>{{ $store.state.moduleB.username }}</p>
+    <!-- 2.2使用B模块的getters数据 -->
+    <p>{{ $store.getters["moduleB/newName"] }}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+    <button @click="actionsFn">actionsFn</button>
   </div>
-  <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<script>
+import { useStore } from "vuex";
+export default {
+  name: "App",
+  setup() {
+    const store = useStore();
+    const mutationsFn = () => {
+      // 2.3提交B模块的修改
+      store.commit("moduleB/updateName");
+    };
+    const actionsFn = () => {
+      // 2.4调用B模块的actions
+      store.dispatch("moduleB/updateName");
+    };
+    return { mutationsFn, actionsFn };
+  },
+};
+</script>
+<style lang="less" scoped>
 </style>
